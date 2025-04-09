6c9aebab082213baf06dff6ec1e834e93de260dc
Maxim Maděra <maxim.madera@tul.cz>
2025-04-09 18:12:18 +0200

    #11 vypis a navigace

--- 03-php-projekt/app/controllers/BookController.php
+++ 03-php-projekt/app/controllers/BookController.php
@@ -38,14 +38,6 @@ class BookController {
                 }
             }
 
-            // Uložení knihy do DB
-            // if ($this->bookModel->create($title, $author, $category, $subcategory, $year, $price, $isbn, $description, $link, $imagePaths)) {
-            //     header("Location: /public/books_list.php");
-            //     exit();
-            // } else {
-            //     echo "Chyba při ukládání knihy.";
-            // }
-
             // Uložení knihy do DB - dočasné řešení, než budeme mít výpis knih
             if ($this->bookModel->create($title, $author, $category, $subcategory, $year, $price, $isbn, $description, $link, $imagePaths)) {
                 header("Location: ../controllers/book_list.php");
--- 03-php-projekt/app/models/Book.php
+++ 03-php-projekt/app/models/Book.php
@@ -9,8 +9,6 @@ class Book {
 
     public function create($title, $author, $category, $subcategory, $year, $price, $isbn, $description, $link, $images) {
         
-        // Dvojtečka označuje pojmenovaný parametr => Místo přímých hodnot se používají placeholdery.
-        // PDO je pak nahradí skutečnými hodnotami při volání metody execute().
         // Chrání proti SQL injekci (bezpečnější než přímé vložení hodnot).
         $sql = "INSERT INTO books (title, author, category, subcategory, year, price, isbn, description, link, images) 
                 VALUES (:title, :author, :category, :subcategory, :year, :price, :isbn, :description, :link, :images)";
